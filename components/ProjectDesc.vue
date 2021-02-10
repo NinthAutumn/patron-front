38c88b82f6cbb60edb6f0f1ceb7eb6c19befa9b7
NinthAutumn <arif.iwamoto@gmail.com>
2021-02-11 00:52:29 +0900

    change

--- components/ProjectDesc.vue
+++ components/ProjectDesc.vue
@@ -13,16 +13,52 @@
     <div class="project-desc__content">
       <p class="project-desc__desc">{{project.description}}</p>
     </div>
+    <div class="project-desc__footer">
+      <div class="project-desc__socials">
+        <div
+          @click="socialHandler(social.link)"
+          class="project-desc__social"
+          v-for="social of socials"
+          :key="social.type"
+        >
+          <fa :icon="socialIcon[social.type]"></fa>
+        </div>
+      </div>
+      <button-card
+        class="project-desc__support"
+        size="big"
+        color="secondary"
+      >サポートする</button-card>
+    </div>
   </div>
 </template>
 
 <script>
 import ShareButton from "@/components/ShareButton";
 export default {
+  data: () => ({
+    socialIcon: {
+      facebook: { iconName: "facebook", prefix: "fab" },
+      line: { iconName: "line", prefix: "fab" },
+      reddit: { iconName: "reddit", prefix: "fab" },
+      twitter: { iconName: "twitter", prefix: "fab" },
+    },
+    socials: [
+      { link: "https://facebook.com", type: "facebook" },
+      { link: "https://line.com", type: "line" },
+      { link: "https://reddit.com", type: "reddit" },
+      { link: "https://twitter.com", type: "twitter" },
+    ],
+  }),
   props: {
     project: {
       type: Object,
       required: true,
+    },
+  },
+  methods: {
+    socialHandler(link) {
+      window.open(link);
     },
   },
   components: {
@@ -40,16 +76,43 @@ export default {
   .flex-divider {
     display: flex;
   }
+  display: flex;
+  flex-direction: column;
   flex-grow: 1;
   margin-left: 1rem;
   transform: translateY(-50px);
   border-radius: 1rem;
   height: 30rem;
+  &__socials {
+    display: flex;
+  }
+  &__social {
+    font-size: 2rem;
+    width: 4rem;
+    cursor: pointer;
+    height: 4rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background: #eee;
+    border-radius: 10rem;
+    margin-right: 1rem;
+  }
+  &__footer {
+    display: flex;
+    align-items: center;
+  }
   &__actions {
     display: flex;
     justify-content: space-between;
     align-items: center;
     margin-bottom: 1rem;
+  }
+  &__support {
+    margin-left: auto;
+  }
+  &__footer {
+    margin-top: auto;
   }
   h2 {
     font-size: 2.5rem;
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,39 +1,42 @@
 export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'オンジン',
-    meta: [{
-        charset: 'utf-8'
+    title: "オンジン",
+    meta: [
+      {
+        charset: "utf-8"
       },
       {
-        name: 'viewport',
-        content: 'width=device-width, initial-scale=1'
+        name: "viewport",
+        content: "width=device-width, initial-scale=1"
       },
       {
-        hid: 'description',
-        name: 'description',
-        content: ''
+        hid: "description",
+        name: "description",
+        content: ""
       }
     ],
-    link: [{
-      rel: 'icon',
-      type: 'image/x-icon',
-      href: '/favicon.ico'
-    }]
+    link: [
+      {
+        rel: "icon",
+        type: "image/x-icon",
+        href: "/favicon.ico"
+      }
+    ]
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: ['~/assets/style/main.scss'],
+  css: ["~/assets/style/main.scss"],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
-    '~/plugins/style',
-    '~/plugins/layout',
-    '~/plugins/intercept',
-    '~/plugins/cropper',
-    '~/plugins/paypal',
-    '~/plugins/paypal-sub',
-    '~/plugins/stripe',
+    "~/plugins/style",
+    "~/plugins/layout",
+    "~/plugins/intercept",
+    "~/plugins/cropper",
+    "~/plugins/paypal",
+    "~/plugins/paypal-sub",
+    "~/plugins/stripe",
     "~/plugins/click"
   ],
 
@@ -45,147 +48,157 @@ export default {
 
   // Modules (https://go.nuxtjs.dev/config-modules)
   modules: [
-    ['@nuxt/http', {
-      proxy: true
-    }],
-    '@nuxtjs/style-resources',
-    '@nuxtjs/device',
-    'nuxt-webfontloader',
-    ['@nuxtjs/toast', {
-      singleton: true
-    }],
+    [
+      "@nuxt/http",
+      {
+        proxy: true
+      }
+    ],
+    "@nuxtjs/style-resources",
+    "@nuxtjs/device",
+    "nuxt-webfontloader",
+    [
+      "@nuxtjs/toast",
+      {
+        singleton: true
+      }
+    ],
     "@nuxtjs/universal-storage",
     [
-      '@nuxtjs/fontawesome',
+      "@nuxtjs/fontawesome",
       {
-        component: 'fa',
-        imports: [{
-            set: '@fortawesome/free-solid-svg-icons',
+        component: "fa",
+        imports: [
+          {
+            set: "@fortawesome/free-solid-svg-icons",
             icons: [
-              'faSyncAlt',
-              'faBookmark',
-              'faThumbsUp',
-              'faComment',
-              'faCommentAlt',
-              'faSortNumericDown',
-              'faLayerGroup',
-              'faGift',
-              'faAngleUp',
-              'faAngleLeft',
-              'faAngleDown',
-              'faAngleRight',
-              'faBolt',
-              'faFlag',
-              'faEnvelope',
-              'faList',
-              'faCog',
-              'faListUl',
-              'faComments',
-              'faLock',
-              'faUnlock',
-              'faScroll',
-              'faEye',
-              'faCube',
-              'faCrown',
-              'faSearch',
-              'faGlobe',
-              'faBook',
-              'faBell',
-              'faSignOutAlt',
-              'faUser',
-              'faBookOpen',
-              'faCoins',
-              'faChartPie',
-              'faMap',
-              'faEllipsisV',
-              'faHome',
-              'faFire',
-              'faChevronLeft',
-              'faChevronRight',
-              'faChevronUp',
-              'faChevronDown',
-              'faImage',
-              'faMugHot',
-              'faEdit',
-              'faCheck',
-              'faSort',
-              'faAddressCard',
-              'faShare',
-              'faCalendar',
-              'faCaretDown',
-              'faQuestionCircle',
-              'faArrowLeft',
-              'faArrowRight',
-              'faBars',
-              'faFeatherAlt',
-              'faCaretRight',
-              'faPen',
-              'faPlay',
-              'faSortNumericDownAlt',
-              'faNewspaper',
-              'faLocationArrow',
-              'faTimes',
-              'faThLarge',
-              'faPlus',
-              'faMinus',
-              'faTint',
-              'faCreditCard',
-              'faPenNib',
-              'faSlidersH',
-              'faThList',
-              'faStream',
-              'faGifts',
-              'faShoppingCart',
-              'faInfoCircle'
+              "faSyncAlt",
+              "faBookmark",
+              "faThumbsUp",
+              "faComment",
+              "faCommentAlt",
+              "faSortNumericDown",
+              "faLayerGroup",
+              "faGift",
+              "faAngleUp",
+              "faAngleLeft",
+              "faAngleDown",
+              "faAngleRight",
+              "faBolt",
+              "faFlag",
+              "faEnvelope",
+              "faList",
+              "faCog",
+              "faListUl",
+              "faComments",
+              "faLock",
+              "faUnlock",
+              "faScroll",
+              "faEye",
+              "faCube",
+              "faCrown",
+              "faSearch",
+              "faGlobe",
+              "faBook",
+              "faBell",
+              "faSignOutAlt",
+              "faUser",
+              "faBookOpen",
+              "faCoins",
+              "faChartPie",
+              "faMap",
+              "faEllipsisV",
+              "faHome",
+              "faFire",
+              "faChevronLeft",
+              "faChevronRight",
+              "faChevronUp",
+              "faChevronDown",
+              "faImage",
+              "faMugHot",
+              "faEdit",
+              "faCheck",
+              "faSort",
+              "faAddressCard",
+              "faShare",
+              "faCalendar",
+              "faCaretDown",
+              "faQuestionCircle",
+              "faArrowLeft",
+              "faArrowRight",
+              "faBars",
+              "faFeatherAlt",
+              "faCaretRight",
+              "faPen",
+              "faPlay",
+              "faSortNumericDownAlt",
+              "faNewspaper",
+              "faLocationArrow",
+              "faTimes",
+              "faThLarge",
+              "faPlus",
+              "faMinus",
+              "faTint",
+              "faCreditCard",
+              "faPenNib",
+              "faSlidersH",
+              "faThList",
+              "faStream",
+              "faGifts",
+              "faShoppingCart",
+              "faInfoCircle"
             ]
           },
           {
-            set: '@fortawesome/free-regular-svg-icons',
-            icons: ['faBookmark', 'faHeart']
+            set: "@fortawesome/free-regular-svg-icons",
+            icons: ["faBookmark", "faHeart"]
           },
           {
-            set: '@fortawesome/free-brands-svg-icons',
+            set: "@fortawesome/free-brands-svg-icons",
             icons: [
-              'faGoogle',
-              'faFacebook',
-              'faCcStripe',
-              'faCcPaypal',
-              'faTwitter',
-              'faPaypal',
-              'faStripeS',
-              'faDiscord',
-              'faPatreon',
+              "faGoogle",
+              "faFacebook",
+              "faCcStripe",
+              "faCcPaypal",
+              "faTwitter",
+              "faPaypal",
+              "faStripeS",
+              "faDiscord",
+              "faPatreon",
               "faLine",
-              "faInstagram"
+              "faInstagram",
+              "faReddit"
             ]
           }
         ]
-      },
-
+      }
     ],
-    '@nuxtjs/pwa',
+    "@nuxtjs/pwa"
   ],
   proxy: {
     "/v1/": {
       target: "http://localhost:5000/",
       pathRewrite: {
-        '^/v1/': '/'
-      },
+        "^/v1/": "/"
+      }
     },
     "/file": {
       target: "https://img.jpmtl.com/",
       pathRewrite: {
-        '^/file': '/'
-      },
+        "^/file": "/"
+      }
     }
   },
   webfontloader: {
     google: {
-      families: ['Noto Sans JP:500&display=swap'] //Loads Lato font with weights 400 and 700
+      families: ["Noto Sans JP:500&display=swap"] //Loads Lato font with weights 400 and 700
     }
   },
   styleResources: {
-    scss: ['~/assets/style/abstract/_variables.scss', '~/assets/style/abstract/_functions.scss']
+    scss: [
+      "~/assets/style/abstract/_variables.scss",
+      "~/assets/style/abstract/_functions.scss"
+    ]
   },
   build: {
     // extractCSS: true,
@@ -206,4 +219,4 @@ export default {
     host: "0.0.0.0",
     port: 8080
   }
-}
+};
